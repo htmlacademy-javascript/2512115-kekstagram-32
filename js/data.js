542549498efe59498d2e4687ea83513beeb21cf5
Alex <fedotok2@gmail.com>
2024-08-14 15:02:33 +0400

    8.14. Открывается и закрывается

--- js/data.js
+++ js/data.js
@@ -1,6 +1,19 @@
-import {getRandomInteger} from './get-rand-int.js';
-import {getRandomArrayElement} from './get-rand-arr-el.js';
-import {getUniqRandomIntFromRange} from './get-uniq-rand-int-from-range.js';
+import { getRandomInteger, getRandomArrayElement, createIdGenerator } from './util.js';
+
+const PICTURE_COUNT = 25;
+const AVATAR_COUNT = 6;
+const LIKE_MIN_COUNT = 15;
+const LIKE_MAX_COUNT = 200;
+const COMMENT_COUNT = 20;
+
+const COMMENT_LINES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
 
 const NAMES = [
   'Андрей',
@@ -13,15 +26,6 @@ const NAMES = [
   'Елена',
 ];
 
-const MESSAGES = [
-  'Всё отлично!',
-  'В целом всё неплохо. Но не всё.',
-  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-];
-
 const DESCRIPTIONS = [
   'Вот как-то так',
   'Просто фото',
@@ -31,25 +35,26 @@ const DESCRIPTIONS = [
   'Без комментариев',
 ];
 
-let authorId = 1;
+const genetateCommentId = createIdGenerator();
+
+const createMessage = () => Array.from(
+  { length: getRandomInteger(1, 2) }, () => getRandomArrayElement(COMMENT_LINES),).join('');
 
 const createComment = () => ({
-  id: authorId++,
-  avatar: `img/avatar-${ getRandomInteger(1, 6) }.svg`,
-  message: `${getRandomArrayElement(MESSAGES) } ${ getRandomArrayElement(MESSAGES)}`,
-  name: getRandomArrayElement(NAMES)});
-
-const createPicture = () => ({
-  id: getUniqRandomIntFromRange(1, 25)(),
-  url: `photos/${ getUniqRandomIntFromRange(1, 25)() }.jpg`,
-  description: getRandomArrayElement(DESCRIPTIONS),
-  likes: getRandomInteger(15, 200),
-  comments: Array.from({length: getRandomInteger(0, 30)}, createComment),
+  id: genetateCommentId(),
+  avatar: `img/avatar-${ getRandomInteger(1, AVATAR_COUNT)}.svg`,
+  message: createMessage(),
+  name: getRandomArrayElement(NAMES)
 });
 
-const createPictures = () => Array.from({length: 25}, createPicture);
-// выводим результат в консоль в виде JSON, иначе не видно значений ключа comments
-// console.log(JSON.stringify(similarObject, null, 2));
-//console.log(similarObject);
+const createPicture = (index) => ({
+  id: index,
+  url: `photos/${index}.jpg`,
+  description: getRandomArrayElement(DESCRIPTIONS),
+  likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
+  comments: Array.from({length: getRandomInteger(0, COMMENT_COUNT)}, createComment),
+});
 
-export {createPictures};
+const getPictures = () => Array.from({length: PICTURE_COUNT}, (_, pictureIndex) => createPicture(pictureIndex + 1));
+
+export { getPictures };
